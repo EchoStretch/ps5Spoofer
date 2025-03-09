4a870387e7da7473fd422191345d2bfef4edfa69
MagicStuff <55626588+MagicStuffCL@users.noreply.github.com>
2025-03-09 03:15:42 -0300

    Added support for versions 5.0 and 5.02

--- main.c
+++ main.c
@@ -10,8 +10,8 @@ typedef struct notify_request {
 
 int sceKernelSendNotificationRequest(int, notify_request_t*, size_t, int);
 unsigned int OFFSET_KERNEL_PS4SDK_55 = 0x023D9048;
-unsigned int OFFSET_KERNEL_PS4SDK_51 = 0x023D8F38;
-unsigned int OFFSET_KERNEL_PS4SDK_43 = 0x022ABE88;
+unsigned int OFFSET_KERNEL_PS4SDK_51_50_502 = 0x023D8F38;
+unsigned int OFFSET_KERNEL_PS4SDK_43 = 0x16ABE88;
 
 int
 main() {
@@ -20,15 +20,18 @@ main() {
   bzero(&req, sizeof req);
   switch(kernel_get_fw_version() & 0xffff0000) {
       case 0x4030000:
-        strncpy(req.message, "The PS4 SDK has been patched to version 99.9 (firm 4.03)", sizeof req.message);
+        strncpy(req.message, "The PS4 SDK patched to 99.9 - MagicStuff", sizeof req.message);
         kernel_setint(KERNEL_ADDRESS_TEXT_BASE + OFFSET_KERNEL_PS4SDK_43, 0x99999999);  
         return sceKernelSendNotificationRequest(0, &req, sizeof req, 0);
+      
+      case 0x5000000:
+      case 0x5020000:
       case 0x5100000:
-        strncpy(req.message, "The PS4 SDK has been patched to version 99.9 (firm 5.1)", sizeof req.message);
-        kernel_setint(KERNEL_ADDRESS_TEXT_BASE + OFFSET_KERNEL_PS4SDK_51, 0x99999999);
+        strncpy(req.message, "The PS4 SDK patched to 99.9 - MagicStuff", sizeof req.message);
+        kernel_setint(KERNEL_ADDRESS_TEXT_BASE + OFFSET_KERNEL_PS4SDK_51_50_502, 0x99999999);
         return sceKernelSendNotificationRequest(0, &req, sizeof req, 0);
       case 0x5500000:
-        strncpy(req.message, "The PS4 SDK has been patched to version 99.9 (firm 5.5)", sizeof req.message);
+        strncpy(req.message, "The PS4 SDK patched to 99.9 - MagicStuff", sizeof req.message);
         kernel_setint(KERNEL_ADDRESS_TEXT_BASE + OFFSET_KERNEL_PS4SDK_55, 0x99999999);  
         return sceKernelSendNotificationRequest(0, &req, sizeof req, 0);
   }
